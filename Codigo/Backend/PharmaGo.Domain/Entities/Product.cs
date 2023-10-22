1b8e9f40c820a6b0b6403b9daf56c8b1b2ad3211
analaura.montesdeoca <al.montesdeoca.2009@gmail.com>
2023-10-22 10:26:34 -0300

    Entity Product.cs

---
+++ Codigo/Backend/PharmaGo.Domain/Entities/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmaGo.Domain.Entities
+{
+    public class Product
+    {
+        private string productName;
+        private string productDescription;
+        private double productPrice;
+        private int productCode;
+
+        public Product()
+        {
+        }
+
+        public Product(int code,string productName, string productDescription, double productPrice)
+        {
+            this.productName = productName;
+            this.productDescription = productDescription;
+            this.productPrice = productPrice;
+            this.productCode = code;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Code { get; set; }
+        public double Price { get; set; }
+    }
+}
